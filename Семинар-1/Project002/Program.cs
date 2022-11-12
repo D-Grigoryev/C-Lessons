85640034728237976babc4db2245439748facf25
dmitriy <dinvest4@gmail.com>
2022-11-12 16:03:33 +0300

    Добавил задачи решенные на первом семинаре

---
+++ Семинар-1/Project002/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напиште программу, которая принимает на вход число и выдает его квадарт (число)
+Console.WriteLine("Введите целое число");
+int num = Convert.ToInt32(Console.ReadLine());
+math(num);
+void math (int x)
+{
+    Console.WriteLine("Квадрат числа x равен: "+x*x);
+}
