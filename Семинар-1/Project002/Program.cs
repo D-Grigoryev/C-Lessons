409fa89660f6acb0ac014006be8c63819c7a89f6
dmitriy <dinvest4@gmail.com>
2022-11-12 20:46:01 +0300

    Добавил в ветку main домашнее задание к 1-у семинару

--- Семинар-1/Project002/Program.cs
+++ Семинар-1/Project002/Program.cs
@@ -1,6 +1,6 @@
 ﻿// Напиште программу, которая принимает на вход число и выдает его квадарт (число)
 Console.WriteLine("Введите целое число");
-int num = Convert.ToInt32(Console.ReadLine());
+int num = Convert.TiInt32(Console.ReadLine());
 math(num);
 void math (int x)
 {
