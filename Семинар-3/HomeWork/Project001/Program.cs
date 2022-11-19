95fc608ee5487d864f6c6c2d20a0eae21b33b378
dmitriy <dinvest4@gmail.com>
2022-11-19 18:48:07 +0300

    Добавлены задачи к 3-у семинару и домашнее задание к нему

---
+++ Семинар-3/HomeWork/Project001/Program.cs
@@ -0,0 +1,47 @@
+﻿
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+//
+//РЕШЕНИЕ ЧЕРЕЗ String
+
+// Console.Write("Введите число: ");
+// string number = Console.ReadLine();
+
+// void CheckingNumber(string number)
+// {
+//   if (number[0]==number[4] || number[1]==number[3])
+//   {
+//     Console.WriteLine($"Число: {number} - палиндром.");
+//   }
+//   else Console.WriteLine($"Число: {number} - НЕ палиндром.");
+// }
+// if (number!.Length == 5)
+// {
+//   CheckingNumber(number);
+// }
+// else Console.WriteLine($"Введи правильное число");
+
+
+//РЕШЕНИЕ БЕЗ STRING
+
+Console.Write("Введите число: ");
+double num = Convert.ToInt32(Console.ReadLine());
+        
+       
+
+ Palindrom(num);
+void Palindrom(double n){
+    double num_length = Math.Floor(Math.Log10(num))+1;
+    double first_num = Math.Floor(num/(Math.Pow(10, 4)));
+    double second_num = Math.Floor((num/(Math.Pow(10, 3))%10));
+    double fourth_num = Math.Floor((num%100)/10);
+    double fifth_num = Math.Floor(num%10);
+    if (first_num==fifth_num&&second_num==fourth_num){
+        Console.WriteLine($"{num} - полниндром");
+    }else{
+        Console.WriteLine($"{num} - не полниндром");
+    }
+}
---
+++ Семинар-3/Project001/Program.cs
@@ -0,0 +1,31 @@
+﻿    // Задача №17. Напишите программу, которая принимает на вход координаты точки (X и Y), 
+    // причем X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+
+    Console.WriteLine ("Введите значение X ");
+    int x = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine ("Введите значение Y ");
+    int y = Convert.ToInt32(Console.ReadLine());
+
+    validEnter(x,y);
+    void validEnter (int x, int y){
+
+    if (x!= 0 && y!=0){
+    cor(x,y);
+    }else Console.WriteLine ("Вы ввели неккоректное значение");
+    }
+
+    void cor (int x, int y){
+    if (x>0 && y>0){
+        Console.WriteLine ("Четверть-1");
+    }
+    if (x>0 && y<0){
+        Console.WriteLine ("Четверть-2");
+    }
+    if (x<0&& y<0){
+        Console.WriteLine ("Четверть-3");
+    }
+    if (x<0&& y>0){
+        Console.WriteLine ("Четверть-4");
+    }
+
+    }
